5b9024933955d8bff3c3e28cd65329b17c885ddb
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-10-26 23:05:15 +0530

    Add files via upload

---
+++ dict_ex1.sh
@@ -0,0 +1,16 @@
+
+declare -A person
+
+person[name]="lokesh"
+person[age]=101
+person[place]="bnglr"
+
+echo ${person[name]}
+
+
+
+
+
+
+
+
--- random.sh
+++ random.sh
@@ -1,22 +1,7 @@
 
+rand=$(( RANDOM%15 ))
 
-n1=$(( RANDOM % 20 ))
-n2=$(( RANDOM % 20 ))
-n3=$(( RANDOM % 20 ))
-n4=$(( RANDOM % 20 ))
-n5=$(( RANDOM % 20 ))
-
-NO_OF_RANDS=5 
-
-echo $n1 $n2 $n3 $n4 $n5
-
-sum=$(( n1+n2+n3+n4+n5 ))
-
-echo "sum: $sum"
-
-avg=$(( sum/NO_OF_RANDS ))
-
-echo "avg: $avg"
+echo $rand
 
 
 
